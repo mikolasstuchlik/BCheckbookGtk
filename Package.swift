65866653719a1f2b0a13bf9b5082740b2bbe3222
Bryce Campbell <tonyhawk2100@gmail.com>
2021-07-27 09:00:40 -0700

    Add SwiftGtk dependency

--- Package.swift
+++ Package.swift
@@ -6,15 +6,15 @@ import PackageDescription
 let package = Package(
     name: "bcheckbook",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "gir2swift", url: "https://github.com/rhx/gir2swift.git", .branch("main")),
+        .package(name: "Gtk", url: "https://github.com/rhx/SwiftGtk.git", .branch("gtk3")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "bcheckbook",
-            dependencies: []),
+            dependencies: ["Gtk"]),
         .testTarget(
             name: "bcheckbookTests",
             dependencies: ["bcheckbook"]),
