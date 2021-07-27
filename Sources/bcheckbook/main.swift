66d7bf62bde8de1af310f488f7546809a870c0bb
Bryce Campbell <tonyhawk2100@gmail.com>
2021-07-27 10:00:36 -0700

    work on getting basic list view.

--- Sources/bcheckbook/main.swift
+++ Sources/bcheckbook/main.swift
@@ -1,11 +1,31 @@
 import Gtk
+import GLibObject
+import CGtk
 
 let status = Application.run(startupHandler: nil) { app in
     let window = ApplicationWindowRef(application: app)
     window.title = "Hello, World!"
     window.setDefaultSize(width: 320, height: 240)
-    let label = LabelRef(str: "Hello, World!")
+    /* let label = LabelRef(str: "Hello, World!")
     window.add(widget: label)
+    window.showAll() */
+    let iterator = TreeIter()
+    let store = ListStore(.string, .string, .boolean, .string, .string, .string, .string, .string)
+    var listView = ListView(model: store)
+    let columns = [
+        ("Date", "text", CellRendererText()),
+        ("Check #", "text", CellRendererText()),
+        ("Reconciled", "active", CellRendererToggle()),
+        ("Vendor", "text", CellRendererText()),
+        ("Memo", "text", CellRendererText()),
+        ("Deposit", "text", CellRendererText()),
+        ("Withdrawal", "text", CellRendererText()),
+        ("Balance", "text", CellRendererText())
+    ].enumerated().map {(i: Int, c:(title: String, kind: PropertyName, renderer: CellRenderer)) in
+        TreeViewColumn(i, title: c.title, renderer: c.renderer, attribute: c.kind)
+    }
+    listView.append(columns)
+    window.add(widget: listView)
     window.showAll()
 }
 
