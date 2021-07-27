da24fad8141aa51d10ac50cd5d86f9d8ded42726
Bryce Campbell <tonyhawk2100@gmail.com>
2021-07-27 09:19:32 -0700

    start working on basic program.

--- Sources/bcheckbook/main.swift
+++ Sources/bcheckbook/main.swift
@@ -1,1 +1,18 @@
-print("Hello, world!")
+import Gtk
+
+let status = Application.run(startupHandler: nil) { app in
+    let window = ApplicationWindowRef(application: app)
+    window.title = "Hello, World!"
+    window.setDefaultSize(width: 320, height: 240)
+    let label = LabelRef(str: "Hello, World!")
+    window.add(widet: label)
+    window.showAll()
+}
+
+guard let status = status else {
+    fatalError("Could not create Application")
+}
+
+guard status == 0 else {
+    fatalErr("Application exited with status \(status)")
+}
